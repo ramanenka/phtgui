a54e1dd1da92b68ce48e9df57522f5cd26a63a33
Vadzim Ramanenka <vramanenka@gmail.com>
2017-01-27 01:10:16 +0100

    Move Trace component to components directory

--- js/router.jsx
+++ js/router.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Router, Route} from 'react-router'
 import TracesList from './components/traces-list/traces-list'
-import Trace from './Trace'
+import Trace from './components/Trace'
 
 export default function createRouter(history) {
   return (
