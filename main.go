2249fff77c66e9a9549fbb4fcaf8ffd08d67e135
Vadzim Ramanenka <vramanenka@gmail.com>
2017-01-28 19:59:21 +0100

    Decode in goroutine through a channel

--- main.go
+++ main.go
@@ -46,63 +46,76 @@ type eventEnd struct {
 	Tsc uint64
 }
 
+func walk(file *os.File) chan interface{} {
+	result := make(chan interface{}, 1000)
+	go func(file *os.File, ch chan interface{}) {
+		// f, err := os.Create("/traces/goprof.prof")
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// pprof.StartCPUProfile(f)
+		// defer pprof.StopCPUProfile()
+		r := bufio.NewReader(file)
+
+		var ev byte
+		for {
+			err := binary.Read(r, binary.LittleEndian, &ev)
+			if err != nil {
+				close(ch)
+				break
+			}
+
+			switch ev {
+			case EventTypeDataString:
+				var stringID uint32
+				binary.Read(r, binary.LittleEndian, &stringID)
+				bytesStr, _ := r.ReadBytes(0)
+				str := string(bytesStr)
+				_ = str
+				//fmt.Println("EVENT_DATA_STRING: string id =", stringID, "string =", str)
+			case EventTypeCompileFile:
+				var e eventCompileFile
+				binary.Read(r, binary.LittleEndian, &e)
+				ch <- &e
+
+			case EventTypeCall:
+				var e eventCall
+				binary.Read(r, binary.LittleEndian, &e)
+				ch <- &e
+
+			case EventTypeICall:
+				var e eventICall
+				binary.Read(r, binary.LittleEndian, &e)
+				ch <- &e
+
+			case EventTypeEnd:
+				var e eventEnd
+				binary.Read(r, binary.LittleEndian, &e)
+				ch <- &e
+
+			default:
+				log.Fatal("Unknown event type: ", ev)
+			}
+		}
+
+	}(file, result)
+	return result
+}
+
 func decode() {
 	t0 := time.Now()
-	var eventsCount uint64
-	events := make([]interface{}, 0, 1E6)
-
 	file, err := os.Open("/traces/phtrace.phtrace")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	r := bufio.NewReader(file)
-	var ev byte
-	for {
-		err := binary.Read(r, binary.LittleEndian, &ev)
-		if err != nil {
-			break
-		}
-		eventsCount++
-
-		switch ev {
-		case EventTypeDataString:
-			var stringID uint32
-			binary.Read(r, binary.LittleEndian, &stringID)
-			bytesStr, _ := r.ReadBytes(0)
-			str := string(bytesStr)
-			_ = str
-			//fmt.Println("EVENT_DATA_STRING: string id =", stringID, "string =", str)
-		case EventTypeCompileFile:
-			var e eventCompileFile
-			binary.Read(r, binary.LittleEndian, &e)
-			//fmt.Println("EVENT_COMPILE_FILE:", e)
-			events = append(events, e)
-		case EventTypeCall:
-			var e eventCall
-			binary.Read(r, binary.LittleEndian, &e)
-			//fmt.Println("EVENT_CALL:", e)
-			events = append(events, e)
-		case EventTypeICall:
-			var e eventICall
-			binary.Read(r, binary.LittleEndian, &e)
-			//fmt.Println("EVENT_ICALL:", e)
-			events = append(events, e)
-		case EventTypeEnd:
-			var e eventEnd
-			binary.Read(r, binary.LittleEndian, &e)
-			//fmt.Println("EVENT_END:", e)
-			events = append(events, e)
-		default:
-			log.Fatal("Unknown event type: ", ev)
-		}
+	for e := range walk(file) {
+		_ = e
 	}
 
 	t1 := time.Now()
 	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
-	fmt.Println("Events count: ", eventsCount)
-	fmt.Println("Events without strings: ", len(events))
 }
 
 func main() {
@@ -138,6 +151,12 @@ func main() {
 			data = append(data, m)
 		}
 		json.NewEncoder(w).Encode(data)
+	})
+
+	http.HandleFunc("/ololo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Decoding events")
+		decode()
+		fmt.Fprintln(w, "Done!")
 	})
 
 	http.ListenAndServe(":8080", nil)
