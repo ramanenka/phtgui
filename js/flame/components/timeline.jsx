f9518eddcccc1a3605376934ea507e63cc86eca0
Vadzim Ramanenka <vramanenka@gmail.com>
2017-02-20 17:46:21 +0300

    Highlight selected area on the timeline

--- js/flame/components/timeline.jsx
+++ js/flame/components/timeline.jsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 
 class TimelineBase extends React.Component {
   render() {
-    let {timeline, traceData, width, height} = this.props,
+    let {timeline, traceData, width, height, tsc0, tsc100} = this.props,
       points = []
 
     if (!timeline) {
@@ -17,10 +17,14 @@ class TimelineBase extends React.Component {
       points.push(`${x} ${y}`)
     }
 
+    tsc100 = (tsc100 - tsc0) / (traceData.tsc_end - traceData.tsc_begin) * 100
+    tsc0 = tsc0 / (traceData.tsc_end - traceData.tsc_begin) * 100
+
     return (
       <g>
-        <rect x="0" y="0" width="100%" height={height} fill="#eff5fb"/>
-        <polygon points={points.join(', ')} stroke="#63a0d4" fill="#9fc5e5"/>
+        <rect x="0" y="0" width="100%" height={height} fill="transparent"/>
+        <polygon points={points.join(', ')} stroke="#63a0d4" fill="#9fc5e5" strokeWidth="0.5"/>
+        <rect x={tsc0 + "%"} y="0" width={tsc100 + "%"} height={height} fill="rgba(99, 160, 212, 0.5)" stroke="#63a0d4" />
       </g>
     )
   }
@@ -30,7 +34,9 @@ const Timeline = connect(
   (state) => ({
     traceData: state.trace.data,
     timeline: state.flame.timeline,
-    width: state.flame.width
+    width: state.flame.width,
+    tsc0: state.flame.tsc0,
+    tsc100: state.flame.tsc100
   }),
   (dispatch) => ({})
 )(TimelineBase)
